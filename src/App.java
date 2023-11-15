128c59676337781d4a751e7cf52c72c970f2ae9c
06776 <hajdarapatrik11@outlook.com>
2023-11-15 20:13:22 +0100

    Temperature in clear code

---
+++ src/App.java
@@ -0,0 +1,13 @@
+/*
+ * Hajdara Patrik, 2023-11-14
+ * Copyright (c) 2023, Hajdara Patrik
+ * Licenc: MIT
+ */
+
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    Temp temperature = new Temp();
+    temperature.celsius();
+  }
+}
---
+++ src/Temp.java
@@ -0,0 +1,65 @@
+/*
+ * Hajdara Patrik, 2023-11-14
+ * Copyright (c) 2023, Hajdara Patrik
+ * Licenc: MIT
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Temp {
+
+  ArrayList<Double> tempDetails;
+
+  public Temp() {
+    Double[] changes = { 2.5, 2.8, 1.2, 0.5, -1.1 };
+    this.tempDetails = new ArrayList<>(Arrays.asList(changes));
+  }
+
+  public void celsius() {
+    String time = "";
+    while (!time.equals("done")) {
+      time = information();
+      lep(time);
+    }
+  }
+
+  public String information() {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Temperature: ");
+    return scan.nextLine();
+  }
+
+  public void lep(String tempStr) {
+    if (!tempStr.equals("done")) {
+      output(tempStr);
+    } else {
+      wl();
+    }
+  }
+
+  public void output(String tempStr) {
+    if (cik(tempStr)) {
+      double temp = Double.parseDouble(tempStr);
+      tempDetails.add(temp);
+    } else {
+      System.err.println("Wrong input! Please enter a number");
+    }
+  }
+
+  public boolean cik(String a) {
+    if (a.matches("[0-9.]+")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public void wl() {
+    for (double changes : this.tempDetails) {
+      System.out.print(changes + " ");
+    }
+    System.out.println();
+  }
+}
